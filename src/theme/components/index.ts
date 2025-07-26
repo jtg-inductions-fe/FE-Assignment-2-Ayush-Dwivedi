a5e181558e66acff52ddf50b1b8add854e748c11
Ayush Dwivedi <ayush.dwivedi@joshtechnologygroup.com>
2025-07-26 18:27:27 +0530

    AD_A2_0: Fixed font render issue

--- src/theme/components/index.ts
+++ src/theme/components/index.ts
@@ -4,26 +4,25 @@ import InterTTF from '@assets/fonts/inter/inter.ttf';
 import InterWOFF from '@assets/fonts/inter/inter.woff';
 import InterWOFF2 from '@assets/fonts/inter/inter.woff2';
 
-/**
- * Font declarations for Inter font family.
- * Used via MUI's CssBaseline to inject global @font-face rules.
- */
-const fontFaceDeclarations = `
-      @font-face {
-        font-display: swap; 
-        font-family: 'Inter';
-        font-style: normal;
-        src: url(${InterWOFF2}) format('woff2'), url(${InterWOFF}) format('woff'), url(${InterTTF}) format('truetype');
-      }
-    `;
-
 export const components: Components = {
     MuiCssBaseline: {
         styleOverrides: {
+            '@font-face': [
+                {
+                    fontFamily: 'Inter',
+                    fontStyle: 'normal',
+                    fontDisplay: 'swap',
+                    fontWeight: '100 900', // if it's a variable font
+                    src: `
+            url(${InterWOFF2}) format('woff2'),
+            url(${InterWOFF}) format('woff'),
+            url(${InterTTF}) format('truetype')
+          `,
+                },
+            ],
             html: {
                 fontSize: '62.5%',
             },
-            fontFaceDeclarations,
         },
     },
 };
